f3cf09bbe11c8f6e9556a36a3fd987c9a4b0661b
Jimmy Baik <jbaik9505@gmail.com>
2025-08-09 17:49:45 +0900

    feat: route parameter 테스트 경로 추가

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import fs from 'node:fs';
 
 const app = express();
 const port = 3002;
@@ -6,6 +7,11 @@ const port = 3002;
 
 app.get('/', (req, res) => {
     res.send("Hello!");
+});
+
+app.get('/params-example/:userSuppliedParameter', (req,res) => {
+    const params = req.params;
+    res.send(params);
 });
 
 app.listen(port);

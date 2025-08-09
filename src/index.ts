e83cc660180af1dfd9206dfd042fa2e4776772f7
Jimmy Baik <jbaik9505@gmail.com>
2025-08-09 20:30:12 +0900

    feat: ejs 템플릿 렌더링 추가

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import fs from 'node:fs';
+import path from 'node:path'
 
 const app = express();
 const port = 3002;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.send("Hello!");

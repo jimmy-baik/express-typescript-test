07da05151ab838fefdf11f402585e2f9064756ae
Jimmy Baik <jbaik9505@gmail.com>
2025-08-09 21:04:08 +0900

    feat: xss 방지 미들웨어 추가. url param을 다시 html로 렌더링 시험

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,31 @@
 import express from 'express';
-import fs from 'node:fs';
+import { xss } from 'express-xss-sanitizer';
 import path from 'node:path'
+import { fileURLToPath } from 'node:url';
 
 const app = express();
 const port = 3002;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(xss());
+
 
 app.get('/', (req, res) => {
     res.send("Hello!");
 });
 
 app.get('/params-example/:userSuppliedParameter', (req,res) => {
-    const params = req.params;
-    res.send(params);
+    let paramsString = '';
+    for (let [k, v] of Object.entries(req.params)) {
+        paramsString += String(k) + ':' + String(v);
+    }
+    res.render('index', {title: 'Params Example', body: paramsString});
 });
 
-app.listen(port);
 
+app.listen(port);

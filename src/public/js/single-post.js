8917ad1733b13b3cb7606095ed076b2f32248090
Jimmy Baik <jbaik9505@gmail.com>
2025-10-11 10:56:01 +0900

    feat: 아티클 웹페이지에 사용자 선호도 추적기능 추가

--- src/public/js/single-post.js
+++ src/public/js/single-post.js
@@ -54,8 +54,75 @@ function toggleLike(postId) {
         });
 }
 
+
+// 게시글 열람이력 추적
+let hasTrackedView = false;
+let timeTrackingStarted = false;
+let timeTrackingTimer = null;
+
+function sendTrackPostViewRequest(postId) {
+    if (hasTrackedView) {
+        return; // 이미 기록이 전송된적이 있다면 아무것도 하지 않는다.
+    }
+    
+    hasTrackedView = true;
+    
+    fetch(`/posts/${postId}/viewed`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('게시글 열람이력 추적에 실패했습니다.');
+        }
+    })
+    .catch(error => {
+        console.error('게시글 열람이력 기록 중 오류가 발생했습니다:', error);
+    });
+}
+
+function startTimeTracking(postId) {
+    // 사용자가 한 페이지에서 N초 이상 아티클을 열람했다면 사용자 선호도를 기록
+    if (timeTrackingStarted) {
+        return;
+    }
+    
+    timeTrackingStarted = true;
+    timeTrackingTimer = setTimeout(() => {
+        sendTrackPostViewRequest(postId);
+    }, 30000); // 30초
+}
+
+function checkScrollPosition(postId) {
+    // 사용자가 특정 기준 이상 스크롤 했다면 사용자 선호도를 기록
+
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const documentHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollPercentage = scrollTop / documentHeight;
+    
+    if (scrollPercentage >= 0.33) { // 1/3 of the page
+        sendTrackPostViewRequest(postId);
+    }
+}
+
 // Event 붙이기
 document.addEventListener('DOMContentLoaded', function() {
+
+    const postId = document.querySelector('[data-post-id]')?.getAttribute('data-post-id');    
+    if (postId) {
+        // 열람 시간 추적 시작
+        startTimeTracking(postId);
+        
+        // 스크롤 추적 이벤트 리스너 시작
+        window.addEventListener('scroll', function() {
+            if (!hasTrackedView) {
+                checkScrollPosition(postId);
+            }
+        });
+    }
+    
     // 삭제 버튼 이벤트 리스너
     const deleteBtn = document.querySelector('.delete-btn');
     if (deleteBtn) {
@@ -69,7 +136,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // 좋아요 버튼 이벤트 리스너
     const likeButtons = document.querySelectorAll('.like-btn');
     likeButtons.forEach(button => {
-        button.addEventListener('click', function() {
+        button.addEventListener('click', function(e) {
             const postId = e.target.getAttribute('data-post-id');
             toggleLike(postId);
         });

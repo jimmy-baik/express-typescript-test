c2fc2dbfd7ce99b0ef11822cf15fa4dcf0c3f90f
Jimmy Baik <jbaik9505@gmail.com>
2025-12-07 00:00:55 +0900

    chore: 미사용중인 로컬 user signup, 로그인 로직 주석처리

--- src/adapters/primary/routes/users/usersApiRouter.ts
+++ src/adapters/primary/routes/users/usersApiRouter.ts
@@ -12,30 +12,30 @@ function isAsciiCharactersOnly(str: string): boolean {
     return /^[\x00-\x7F]*$/.test(str);
 }
 
-// user 생성
-router.post('/', async (req, res, next) => {
-    if (!req.body || !req.body.username || !req.body.password) {
-        return res.status(400).json({
-            error: '잘못된 요청입니다.',
-            message: '아이디와 비밀번호를 입력해주세요.'
-        });
-    }
+// user 생성 - 지금은 사용하지 않음
+// router.post('/', async (req, res, next) => {
+//     if (!req.body || !req.body.username || !req.body.password) {
+//         return res.status(400).json({
+//             error: '잘못된 요청입니다.',
+//             message: '아이디와 비밀번호를 입력해주세요.'
+//         });
+//     }
 
-    if (!isAsciiCharactersOnly(req.body.username)) {
-        return res.status(422).json({
-            error: '잘못된 요청입니다.',
-            message: '아이디는 영문자와 숫자만 사용할 수 있습니다.'
-        });
-    }
+//     if (!isAsciiCharactersOnly(req.body.username)) {
+//         return res.status(422).json({
+//             error: '잘못된 요청입니다.',
+//             message: '아이디는 영문자와 숫자만 사용할 수 있습니다.'
+//         });
+//     }
 
-    const fullname = req.body.fullname ? req.body.fullname : null;
+//     const fullname = req.body.fullname ? req.body.fullname : null;
 
-    try {
-        await usersRepository.createUser(req.body.username, req.body.password, fullname);
-        res.redirect('/login');
-    } catch (err) {
-        next(err);
-    }
-});
+//     try {
+//         await usersRepository.createUser(req.body.username, req.body.password, fullname);
+//         res.redirect('/login');
+//     } catch (err) {
+//         next(err);
+//     }
+// });
 
 export default router;

93d7876ed63edff1058bb440dcf5bc5daf9dd2da
Jimmy Baik <jbaik9505@gmail.com>
2025-12-13 22:34:44 +0900

    fix: meilisearch index 초기화가 불완전한 경우 대응 - 이미 존재하는 인덱스이더라도 설정 업데이트

--- src/adapters/secondary/searchengine/meilisearch.ts
+++ src/adapters/secondary/searchengine/meilisearch.ts
@@ -24,9 +24,9 @@ export class MeilisearchAdapter implements ISearchEngine {
 
     async initializeIndex(): Promise<void> {
         try {
-            // index가 이미 존재하는지 확인한다. 있으면 초기화를 중단하고 바로 종료
+            // index가 이미 존재하는지 확인한다.
             await this.client.getIndex(this.indexName);
-            return;
+            console.log('meilisearch index가 존재합니다.');
         } catch (error) {
             // index가 존재하지 않으면 생성한다.
             console.log('meilisearch index가 존재하지 않는 것 같습니다. 생성합니다..');
